b5cb720146c8c167ebe56233de472e9e75a2386d
fakubwoy <farhaan.khan2022@vitstudent.ac.in>
2024-10-11 15:01:00 +0530

    minor fixes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// server/server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -14,23 +13,19 @@ app.use(bodyParser.json());
 app.post('/compile', (req, res) => {
     const asmCode = req.body.code;
 
-    // Write the assembly code to a file
     fs.writeFileSync('temp.asm', asmCode);
 
-    // Compile the assembly code using nasm and ld
     exec('nasm -f elf64 temp.asm && ld -o output temp.o', (error, stdout, stderr) => {
         if (error) {
             console.error(`Error: ${stderr}`);
             return res.status(500).send('Compilation error');
         }
 
-        // Send the compiled executable back to the client
         res.download('output', 'output', (err) => {
             if (err) {
                 console.error(err);
             }
 
-            // Clean up generated files
             fs.unlinkSync('temp.asm');
             fs.unlinkSync('output');
             fs.unlinkSync('temp.o');
